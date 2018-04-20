7bdd5b4eb45250656cdea299575fd509297c7623
iwest <bokthaku@gmail.com>
2018-04-20 18:29:54 -0400

    Add basic discord link handling

--- util/js/centralAuth.js
+++ util/js/centralAuth.js
@@ -112,6 +112,13 @@
 
                     return initialized;
                 }
+            },
+
+            discord: {
+                toLogin: function() {
+                    window.open('https://discordapp.com/login?redirect_to=' +
+                        encodeURIComponent('/oauth2/authorize?client_id=436178201329401857&scope=identify&redirect_uri=http://serpens.house/&response_type=token&state=' + encodeURIComponent(window.location.hash.substring(1)));
+                }
             }
         };
 
