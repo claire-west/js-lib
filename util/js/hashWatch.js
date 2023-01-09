3367562c6f7d1d004d8f22095ca77b73448a4c6d
Claire West <bokthaku@gmail.com>
2023-01-08 20:05:26 -0500

    Add hashWatch.js

---
+++ util/js/hashWatch.js
@@ -0,0 +1,28 @@
+(function(dynCore) {
+    dynCore.declare('lib.hashWatch', function(modules) {
+        var handlers = [];
+
+        var onHashChange = function() {
+            var args = window.location.hash.substr(1).split('/');
+            for (var i = 0; i < handlers.length; i++) {
+                if (handlers[i].condition.call(this, args)) {
+                    handlers[page][i].apply(this, args);
+                }
+            }
+        };
+
+        window.addEventListener("hashchange", onHashChange);
+
+        return function(condition, handler, noinit) {
+            handlers = handlers || [];
+            handlers.push({
+                condition: condition,
+                fn: handler
+            });
+            if (!noinit) {
+                var args = window.location.hash.substr(1).split('/');
+                handler.apply(this, args);
+            }
+        };
+    });
+})(window.dynCore);
